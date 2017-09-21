b5b1fb88243f5e76ad1535ea59a3229a29740b2c
suryaprasanna <suryayalla@ufl.edu>
2017-09-21 11:54:01 -0400

    add integration tests for routes

---
+++ test/integration/test-route-subtraction.js
@@ -0,0 +1,15 @@
+var supertest = require('supertest'),
+assert = require('assert'),
+app = require('../../app');
+
+exports.addition_should_respond_with_a_numeric_result = function(done){
+  supertest(app)
+  .get('/subtract?a=5&b=4')
+  .expect(200)
+  .end(function(err, response){
+    assert.ok(!err);
+    assert.ok(typeof response.body.result === 'number');
+    return done();
+  });
+};
+
