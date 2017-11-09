042d5dc54631f6360b68caccbd429577b437c01e
suryaprasanna <prasannakumar006@gmail.com>
2017-11-08 20:05:42 -0500

    support answer and questions api

--- app.js
+++ app.js
@@ -11,13 +11,16 @@ mongoose.connection.on('connected', function(req,res) {
 
 
 const userController = require('./controller/user_controller');
+const questionController = require('./controller/question_controller');
+const answerController = require('./controller/answer_controller');
 
 const bp = require('body-parser');
 app.use(bp.json());
 
 // All the /users are routed to userController.
 app.use('/users', userController);
-
+app.use('/question', questionController);
+app.use('/answer', answerController);
 
 app.listen(3000, function(){
   console.log('App is now listening');
