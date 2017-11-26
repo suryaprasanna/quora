3b9f15b040ccc460d646b02d46608e61eb22f001
suryaprasanna <prasannakumar006@gmail.com>
2017-11-26 09:45:33 -0500

    add functionality of follow question in data layer

--- data/follows.js
+++ data/follows.js
@@ -4,11 +4,11 @@ var Promises = require('promise');
 var question = require('../model/question');
 var topic = require('../model/topic');
 var user = require('../model/user');
+var activity = require('../model/activity');
 
 module.exports = {
 
 	followQuestion : function(qid, u) {
-
 		var promise = new Promise((resolve, reject) => {
 			resolve(question.findById(qid));
 		}).then((q) => {
@@ -28,6 +28,24 @@ module.exports = {
 				} else {
 					resolve(q);
 				}
+			}).then((q1) => {
+				var promise = new Promise((resolve, reject) => {
+					if (!f) {
+						var a = new activity();
+						a.doc = "questions";
+						a.doc_id = q1._id;
+						a.type = "Started following the question.";
+						a.user_id = u._id;
+						resolve(a.save());
+					} else {
+						resolve(q1);
+					}
+				})
+			}).then((data) => {
+				var promise = new Promise((resolve, reject) => {
+					resolve(u);
+				});
+				return promise;
 			});
 			return promise;
 		});
@@ -35,6 +53,8 @@ module.exports = {
 	},
 
 	followTopic : function(tid, u) {
+		var res = u;
+		var t_final = new topic();
 		var promise = new Promise((resolve, reject) => {
 			resolve(topic.findById(tid));
 		}).then((t) => {
@@ -54,10 +74,47 @@ module.exports = {
 				} else {
 					resolve(t);
 				}
+			}).then((t2) => {
+				t_final = t2;
+				var ts = u.topics;
+				var f1 = fase;
+				for (int i =0; i< ts.length; i++) {
+					if (ts[i] === u) {
+						f = true;
+					}
+				}
+
+				var promise = new Promise((resolve, reject) => {
+					if (!f) {
+						ts.push(t2);
+						u.topics = ts;
+						resolve(u.save());
+					} else {
+						resolve(u);
+					}
+				});
+				return promise;
+			}).then((u) => {
+				var promise = new Promise((resolve, reject) => {
+					if (!f) {
+						var a = new activity();
+						a.doc = "topics";
+						a.doc_id = t_final._id;
+						a.type = "Started following the topic.";
+						a.user_id = u._id;
+						resolve(a.save());
+					} else {
+						resolve(t1);
+					}
+				})
+			}).then((data) => {
+				var promise = new Promise((resolve, reject) => {
+					resolve(u);
+				});
+				return promise;
 			});
-			return promise;
 		});
-		return proimise;
+		return promise;
 	},
 
 	followUser : function(user_id, u) {
@@ -95,6 +152,7 @@ module.exports = {
 		});
 		return promise;
 	},
+
 	unfollowQuestion : function(qid, u) {
 		var promise = new Promise((resolve, reject) => {
 			resolve(question.findById(qid));
